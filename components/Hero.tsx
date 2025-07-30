d4d2b61e1d4978b7c745a032222b3bcb6ab4d750
Rupesh Vavilla <137624310+Vavilla-Rupesh@users.noreply.github.com>
2025-07-30 18:12:31 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,60 +1,3 @@
-// import React from "react";
-// import { Spotlight } from "./ui/Spotlight";
-// import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-// import MagicButton from "./ui/MagicButton";
-// import { FaLocationArrow } from "react-icons/fa6";
-
-// const Hero = () => {
-//   return (
-//     <div className="pb-20 pt-36">
-//       <div>
-//         <Spotlight
-//           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-//           fill="white"
-//         />
-//         <Spotlight
-//           className="top-10 left-full h-[80vh] v-[50vw]"
-//           fill="purple"
-//         />
-//         <Spotlight className="top-28 left-80 h-[80vh] v-[50vw]" fill="blue" />
-//       </div>
-//       <div
-//         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-//        absolute top-0 left-0 flex items-center justify-center"
-//       >
-//         <div
-//           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-//          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-//         />
-//       </div>
-
-//       <div className="flex justify-center relative my-20 z-10">
-//         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center space-y-6">
-//           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 lg:max-w-none lg:whitespace-nowrap">
-//             Blending sleek frontend with strong backend for progress
-//           </p>
-//           <TextGenerateEffect
-//             words="Designing with intent coding with focus and continuously evolving"
-//             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-//           />
-
-//           <p className="text-center md:tracking-wider text-sm md:text-lg lg:text-2xl">
-//             Hi! I&apos;m Vavilla Rupesh, a frontend-focused full-stack learner
-//           </p>
-//           <a href="#about">
-//             <MagicButton
-//               title={"Explore my builds"}
-//               icon={<FaLocationArrow />}
-//               position={"right"}
-//             />
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -78,7 +21,10 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="pb-20 pt-36 relative">
+    <div
+  id="home"
+  className="relative min-h-screen scroll-mt-36 pt-40 pb-20"
+>
       {/* Spotlights */}
       <div>
         <Spotlight
